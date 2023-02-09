f02ae551dc46c9644a5b5c70d8731ebdf7d838f1
Lennox Charles <lennyaiko17@gmail.com>
2023-02-09 17:18:51 +0100

    can now fetch data from API

--- src/pages/AdvocatePage.js
+++ src/pages/AdvocatePage.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const AdvocatePage = () => {
-  return (
+
+    const params = useParams()
+
+    const username = params.username
+
+    return (
     <div>
-        <h1>AdvocatePage</h1>
+        <h1>Advocate Page {username}</h1>
     </div>
-  )
+    )
 }
 
 export default AdvocatePage
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const HomePage = () => {
-  return (
+
+    const [advocates, setAdvocates] = useState([])
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    let getData = async () => {
+        let response = await axios.get('http://localhost:8000/advocates/')
+        setAdvocates(response.data)
+    }
+
+    return (
     <div>
         <h1>HomePage</h1>
+
+        <div>
+            {advocates.map((advocate, index) => (
+                
+                <div key={index}>
+                    <strong>{advocate.username}</strong>
+                    <Link to={`/advocate/${advocate.username}`}>View</Link>
+                </div>
+            ))}
+        </div>
     </div>
-  )
+    )
 }
 
 export default HomePage
